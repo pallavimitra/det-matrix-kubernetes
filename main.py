6632ed4b993aaa2f81e0370b85dcf653d1e3d767
PALLAVI MITRA <pallavi.mitra1992@gmail.com>
2020-06-01 23:51:50 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import numpy as np
+app = Flask(__name__)
+
+@app.route("/inv")
+def invmat():
+    n = 100
+    m = np.random.rand(n, n)
+	inv_m =  np.linalg.inv(m)
+	return inv_m
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
