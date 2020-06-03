6e9cfc030936a3c1815b39d4f7c963be61cf04cb
PALLAVI MITRA <pallavi.mitra1992@gmail.com>
2020-06-03 12:06:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from flask import Flask, jsonify
 import numpy as np
 app = Flask(__name__)
 
-@app.route('/inv', methods=['POST'])
-def invmat():
+@app.route('/det', methods=['POST'])
+def detmat():
     n = 100
     m = np.random.rand(n, n)
-    inv_m =  np.linalg.det(m)
-    return jsonify({'result': inv_m})
+    det_m =  np.linalg.det(m)
+    return jsonify({'result': det_m})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
